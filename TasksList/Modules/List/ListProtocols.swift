f74161c696e24f6468e9057865fb28ac76a53458
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-09-15 21:57:08 +0300

    implementing-contracts

--- TasksList/Modules/List/ListProtocols.swift
+++ TasksList/Modules/List/ListProtocols.swift
@@ -8,45 +8,45 @@
 import Foundation
 
 protocol ListViewInput: AnyObject {
-    func showCell()
-    func showPopup()
+    func show(_ list: ListModels.LoadTasks.ViewModel)
+    func showCell(_ viewModel: ListModels.LoadTasks.ViewModel.ListItemViewModel)
+    func showPopup(for id: UUID)
     func showIsLoading()
     func showError()
 }
 
 protocol ListViewOutput {
     func viewDidLoad()
-    func didSelectItem()
     func didTapAddButton()
-    func didTapEditButton()
-    func didTapShareButton()
-    func didTapDeleteButton()
-    func didHoldTaskCell()
+    func didSelectItem(with id: UUID)
+    func didTapEditButton(for id: UUID)
+    func didTapShareButton(for id: UUID)
+    func didTapDeleteButton(for id: UUID)
+    func didHoldTaskCell(for id: UUID)
 }
 
 protocol ListInteractorInput {
-    func loadItems()
-    func createItem()
-    func deleteItem()
-    func editItem()
-    func shareItem()
+    func loadItems(request: ListModels.LoadTasks.Request)
+    func createItem(request: ListModels.LoadTasks.Request)
+    func deleteItem(request: ListModels.LoadTasks.Request)
+    func editItem(request: ListModels.LoadTasks.Request)
+    func shareItem(request: ListModels.LoadTasks.Request)
 }
 
 protocol ListInteractorOutput: AnyObject {
-    func didLoadItems()
-    func didFail()
-    func didCreateItem()
-    func didDeleteItem()
-    func didEditItem()
-    func didShareItem()
+    func didLoadItems(response: ListModels.LoadTasks.Response)
+    func didCreateItem(response: ListModels.CreateTask.Response)
+    func didDeleteItem(response: ListModels.DeleteTask.Response)
+    func didEditItem(response: ListModels.EditTask.Response)
+    func didShareItem(response: ListModels.ShareTask.Response)
 }
 
 protocol ListWorkerInput {
     func fetchItems()
     func createItem()
-    func deleteItem()
-    func editItem()
-    func shareItem()
+    func deleteItem(with id: UUID)
+    func editItem(with id: UUID)
+    func shareItem(with id: UUID)
 }
 
 protocol ListRouterInput {
