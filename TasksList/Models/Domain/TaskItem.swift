dd3f0a5a0a0fba94a153ba6b29ea01b3961555e6
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-09-15 19:53:01 +0300

    Move domain task model

---
+++ TasksList/Models/Domain/TaskItem.swift
@@ -0,0 +1,16 @@
+//
+//  TaskItem.swift
+//  TasksList
+//
+//  Created by Михаил Рычагов on 15.09.2025.
+//
+
+import Foundation
+
+struct TaskItem {
+    let id: UUID
+    let title: String
+    let description: String?
+    let isDone: Bool
+    let date: Date
+}
---
+++ TasksList/Modules/List/ListRouter/ListRouter.swift
@@ -0,0 +1,11 @@
+//
+//  ListRouter.swift
+//  TasksList
+//
+//  Created by Михаил Рычагов on 14.09.2025.
+//
+
+final class ListRouter: ListRouterInput {
+    // MARK: - Properties
+    weak var viewController: ListViewInput?
+}
